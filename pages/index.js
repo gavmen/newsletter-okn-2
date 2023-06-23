330e6a2f673f53d1f2c84a83d434e1a14e7fbf79
Gabriel Mendonca <gabriel.mendonca@okn.com.br>
2023-06-23 17:32:37 -0300

    mais coisa

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ class NewsletterPage extends React.Component {
     this.state = {
       email: '',
       subscribed: false,
+      emailError: ''
     };
   }
 
@@ -15,15 +16,25 @@ class NewsletterPage extends React.Component {
   }
 
   handleEmailChange = (event) => {
-    this.setState({ email: event.target.value });
+    const email = event.target.value;
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    this.setState({
+      email: email,
+      emailError: isValidEmail ? '' : 'Valid email required'
+    });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    // Perform any necessary form submission logic, e.g., send data to backend
+    const { email } = this.state;
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-    // Simulating a successful subscription for demonstration purposes
-    this.setState({ subscribed: true });
+    if (isValidEmail) {
+      this.setState({ subscribed: true, emailError: '' });
+    } else {
+      this.setState({ emailError: 'Valid email required' });
+    }
   };
 
   renderContent() {
@@ -32,18 +43,20 @@ class NewsletterPage extends React.Component {
     if (subscribed) {
       return (
         <>
-        <div className={`${styles.subscribed} ${subscribed ? styles.show : ''}`}>
-          <img src="/icon-success.svg" alt="Confirmation icon" className={styles.subscribed__image} />
-          <h2 className={styles.subscribed__title}>Thanks for subscribing</h2>
-          <p className={styles.subscribed__text}>
-            A confirmation mail has been sent to <strong>{email}</strong>. Please open it and click the button inside
-            to confirm your subscription.
-          </p>
-          <button className={styles.subscribed__button} onClick={() => this.setState({ subscribed: false })}>
-            Return to newsletter
-          </button>
-        </div>
-      </>
+          <div className={`${styles.subscribed} ${subscribed ? styles.show : ''}`}>
+            <div>
+              <img src="/icon-success.svg" alt="Confirmation icon" className={styles.subscribed__image} />
+              <h2 className={styles.subscribed__title}>Thanks for subscribing!</h2>
+              <p className={styles.subscribed__text}>
+                A confirmation mail has been sent to <strong>{email}</strong>. Please open it and click the button inside
+                to confirm your subscription.
+              </p>
+            </div>
+            <button className={styles.subscribed__button} onClick={() => this.setState({ subscribed: false })}>
+              Dismiss Message
+            </button>
+          </div>
+        </>
       );
     } else {
       return (
@@ -70,7 +83,7 @@ class NewsletterPage extends React.Component {
   }
 
   render() {
-    const { email, subscribed } = this.state;
+    const { email, subscribed, emailError } = this.state;
 
     return (
       <section className={`${styles.news__container} ${subscribed ? styles.expanded : ''}`}>
@@ -80,11 +93,14 @@ class NewsletterPage extends React.Component {
           </div>
           {!subscribed && (
             <div className={styles.news__form_container}>
-              <p className={styles.news__form_text}>Enter your email address:</p>
+              <div className={styles.news__form_box}>
+                <p className={styles.news__form_text}>Enter your email address:</p>
+                {emailError && <p className={styles.news__form_error}>{emailError}</p>}
+              </div>
               <form className={styles.news__form_form} onSubmit={this.handleSubmit}>
                 <input
                   type="email"
-                  className={styles.news__form_input}
+                  className={`${styles.news__form_input} ${emailError ? styles.error : ''}`}
                   placeholder="email@company.com"
                   value={email}
                   onChange={this.handleEmailChange}
@@ -99,6 +115,9 @@ class NewsletterPage extends React.Component {
         </div>
         {!subscribed && (
           <img src="./illustration-sign-up-desktop.svg" alt="Newsletter image" className={styles.news__image} />
+        )}
+        {!subscribed && (
+          <img src="./illustration-sign-up-mobile.svg" alt="Newsletter image" className={styles.news__image_mobile} />
         )}
       </section>
     );
